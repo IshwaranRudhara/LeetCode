cde7e06e8aae12135033bd35632ce12c78137e14
Ishwaran <39009926+IshwaranRudhara@users.noreply.github.com>
2023-01-03 23:15:49 +0530

    Easy Problems

---
+++ 290 Word Pattern.js
@@ -0,0 +1,44 @@
+/**
+ * Problem link:
+ * 290-Word Pattern
+ * Solution: Hash, string
+ *
+ * Given a pattern and a string s, find if s follows the same pattern.
+ * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+ *
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {
+  const hash = {};
+  const str = s.split(" ");
+  const len = pattern.length;
+  if (len != str.length) {
+    return false;
+  }
+
+  for (let i = 0; i < len; i++) {
+    if (hash[pattern[i]]) {
+      if (hash[pattern[i]] !== str[i]) {
+        return false;
+      }
+    } else if (Object.values(hash).indexOf(str[i]) > -1) {
+      return false;
+    } else {
+      hash[pattern[i]] = str[i];
+    }
+  }
+
+  return true;
+};
+
+console.log("result 1: ", wordPattern("abba", "dog cat cat dog")); // true
+console.log("result 2: ", wordPattern("abba", "dog dog dog dog")); // false
