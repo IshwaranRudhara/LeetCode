57e15baa3df01df9008a744029b1b8e4580faf20
Ishwaran <39009926+IshwaranRudhara@users.noreply.github.com>
2023-01-04 23:45:46 +0530

    Create 2244 Minimum Rounds to Complete All Tasks.js

---
+++ 2244 Minimum Rounds to Complete All Tasks.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} tasks
+ * @return {number}
+ */
+var minimumRounds = function (tasks) {
+  let map = {};
+  let ans = 0;
+  for (let e of tasks) {
+    if (!map[e]) {
+      map[e] = 1;
+    } else {
+      map[e] += 1;
+    }
+  }
+  for (let e in map) {
+    if (map[e] == 1) {
+      return -1;
+    } else {
+      ans += Math.ceil(map[e] / 3);
+    }
+  }
+  return ans;
+};
